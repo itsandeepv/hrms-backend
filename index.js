7953f42b162fb1260901c2566b82696034d40d6c
cetsandeepwork <159258159+cetsandeepwork@users.noreply.github.com>
2024-11-29 11:06:53 +0530

    socket changed added

--- index.js
+++ index.js
@@ -86,14 +86,19 @@ db.once("open", () => {
 
 
 // WebSocket connection
+
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
   socket.on("userDetails", async (data) => {
     try {
       // Fetch leads based on user details and follow-up date
-      const leads = data?.role === "admin" ? await NewLeads.find({userId: data?._id}) : await NewLeads.find({leadAssignTo: data?._id})
+      const leads = data?.role === "admin" ? await NewLeads.find({
+        $or: [
+          { companyId: data?._id },
+          { userId: data?._id }
+        ]
+      }) : await NewLeads.find({leadAssignTo: data?._id})
       const filteredLeads = data?.role === "admin" ? leads.filter(lead => isToday(lead.nextFollowUpDate) && (lead?.leadAssignTo===undefined || lead?.leadAssignTo==="")) : leads.filter(lead => isToday(lead.nextFollowUpDate))
-     
       // Emit notifications for the filtered leads
       const newData = filteredLeads.map(lead => {return {
         message: 'This is a reminder for your follow-up scheduled for today with ' + lead.senderName,
@@ -110,6 +115,31 @@ io.on("connection", (socket) => {
     console.log("User disconnected");
   });
 });
+
+// io.on("connection", (socket) => {
+//   console.log("A user connected", socket.id);
+//   socket.on("userDetails", async (data) => {
+//     try {
+//       // Fetch leads based on user details and follow-up date
+//       const leads = data?.role === "admin" ? await NewLeads.find({userId: data?._id}) : await NewLeads.find({leadAssignTo: data?._id})
+//       const filteredLeads = data?.role === "admin" ? leads.filter(lead => isToday(lead.nextFollowUpDate) && (lead?.leadAssignTo===undefined || lead?.leadAssignTo==="")) : leads.filter(lead => isToday(lead.nextFollowUpDate))
+     
+//       // Emit notifications for the filtered leads
+//       const newData = filteredLeads.map(lead => {return {
+//         message: 'This is a reminder for your follow-up scheduled for today with ' + lead.senderName,
+//         lead
+//       }});
+//       socket.emit('followUpNotification', newData);
+//     } catch (err) {
+//       console.error('Error fetching leads: ', err);
+//     }
+//   });
+
+//   io.emit('triggerUserDetails');
+//   socket.on("disconnect", () => {
+//     console.log("User disconnected");
+//   });
+// });
 
 // Start the server and listen for incoming requests
 const port = process.env.PORT || 5001;
